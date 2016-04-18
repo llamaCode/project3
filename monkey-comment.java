45e829c94bac0cf06349186bc4d4896f6ab9d35d
Prof. B. A. Martin <bam59cst112eve@suffolk.li>
2016-04-18 11:52:21 -0400

    Create monkey-comment.java

---
+++ monkey-comment.java
@@ -0,0 +1,65 @@
+/**  Project 3.
+  Animated monkey climbs up and down a ladder (at right side).
+  Flocks of birds fly across the screen from left to right.
+ 
+  [BIRDS} button starts a new flock of birds flying across from left to right.
+      Random height for the leader bird (from top of ladder to halfway down).
+      Random number of birds in the flock (from two to seven) follow the leader.
+      Each bird in the flock is slightly smaller the one ahead of it and flies lower.
+
+  BLUEBIRDS & VULTURES:
+      Every other flock is all blubirds or all vultures. 
+      Bluebirds are good:  score points when leader is caught by monkey.
+      Vultures are bad:  lose points when leader hits the monkey.
+
+  MONKEY:
+    Whenever space bar is pressed, the monkey moves one step,
+    changing position of arms & legs with each step up or down,
+    changing direction at top or bottom of the ladder.
+
+
+
+    
+           #             #
+           #             #
+           #             #
+           #@============# 
+           # \ ( o o )   #
+           #  \_( - )_  #
+           #   [      ]\ #
+           #   [      ] \#
+           #@==[      ]=@#             
+           # \ [      ]  #
+           #  \[______]  #
+           #          \  #            
+           #           \ #
+           #============@#
+           #             #
+           #             #
+*/
+/*
+
+ @
+  \         
+   \__O__
+    [   ]\
+@   [ M ] \ 
+ \  [   ]  @
+  \_[___]_
+          \
+           \
+            @
+
+
+           @
+          /
+    __O_ /
+   /[   ]
+  / [ M ]   @
+ @  [   ]  /
+   _[___]_/
+  /
+ /
+@
+
+*/
